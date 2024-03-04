978024a5bc22e00efcb31603ab95eb31fbce132e
Jagdeesh Bhandari <jagdeeshbhandari05@gmail.com>
2024-03-04 23:37:33 +0530

    slider added

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ const sidebar = document.querySelector('.sidebar')
     times.addEventListener("click",()=>{
         sidebar.style.left = '-100%';
     })
+
+
+
+    // acordino
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+
+
+//loader
+window.addEventListener('load', function() {
+  document.body.classList.add('loaded');
+});
